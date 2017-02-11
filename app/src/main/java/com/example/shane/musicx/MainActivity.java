93bb69758420528027386e22205cf8fcc4f0703e
shanebennett99 <shanebennett99@yahoo.com>
2017-02-11 21:16:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shane/musicx/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shane.musicx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
